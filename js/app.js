9c32bed9ca5bfbab522bf3c2d193bd4044ac542b
majka521 <521majka@gmail.com>
2021-10-22 14:04:15 +0200

    furry game

--- js/app.js
+++ js/app.js
@@ -4,6 +4,3 @@ import Game from "./game";
 const game = new Game();
 game.startGame();
 game.initEvent();
-
-// window.localStorage.setItem("bestScore", game.bestScore);
-// game.bestScore = window.localStorage.getItem("bestScore");
