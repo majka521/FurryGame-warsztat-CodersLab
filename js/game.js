a4f056b1eb7713f53683fdc18958f9e2c19c68f4
majka521 <521majka@gmail.com>
2021-12-27 19:29:02 +0100

    localstorage

--- js/game.js
+++ js/game.js
@@ -77,12 +77,12 @@ class Game {
       if (this.bestScore < this.score) {
         this.bestScore = this.score;
       }
-      if (sessionStorage.getItem("bestScore") == null) {
-        sessionStorage.setItem("bestScore", this.bestScore);
-      } else if (sessionStorage.getItem("bestScore") < this.bestScore) {
-        sessionStorage.setItem("bestScore", this.bestScore);
+      if (localStorage.getItem("bestScore") == null) {
+        localStorage.setItem("bestScore", this.bestScore);
+      } else if (localStorage.getItem("bestScore") < this.bestScore) {
+        localStorage.setItem("bestScore", this.bestScore);
       }
-      document.getElementById("best-score").innerText = sessionStorage.getItem("bestScore");
+      document.getElementById("best-score").innerText = localStorage.getItem("bestScore");
       return true;
     }
     return false;
